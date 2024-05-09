71eef02f960218fb06fdf77e1997ea8fe498bb07
PremKumarArulSF <166098990+PremKumarArulSF@users.noreply.github.com>
2024-05-09 23:56:46 +0530

    Adding Medical API without DB

---
+++ MedicalAPI/Data/Medical.cs
@@ -0,0 +1,10 @@
+namespace MedicalAPI;
+
+public class Contacts
+{
+    public string ID{get;set;}
+    public string  Name{get;set;}
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    
+}
